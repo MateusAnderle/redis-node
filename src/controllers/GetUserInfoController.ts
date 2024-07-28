db9d951e0336c351e39577ddd639afa46b525278
Mateus Anderle <matsanderle@gmail.com>
2024-07-27 21:26:47 -0300

    Update user controller

--- src/controllers/GetUserInfoController.ts
+++ src/controllers/GetUserInfoController.ts
@@ -6,17 +6,16 @@ export class GetUserInfoController {
   async handle(request: Request, response: Response) {
     const { userId } = request;
 
-    // const clientConnection = await createConnection();
+    const clientConnection = await createConnection();
 
     console.time();
 
     const userRedis = await getRedis(`user-${userId}`);
     const user = JSON.parse(userRedis);
 
-    // const { rows } = await clientConnection.query(
-    //   `SELECT * FROM USERS WHERE ID  = $1 LIMIT 1`,
-    //   [userId]
-    // );
+    await clientConnection.query(`SELECT * FROM USERS WHERE ID  = $1 LIMIT 1`, [
+      userId,
+    ]);
 
     console.timeEnd();
 
